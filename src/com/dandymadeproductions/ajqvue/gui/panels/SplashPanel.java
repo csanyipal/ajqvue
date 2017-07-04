8c91e0f5073ab4ea232e7a784d7a47d04f02bad2
danap <danap@dandymadeproductions.com>
2017-07-04 11:27:04 -0600

    Copyright Update.

--- src/com/dandymadeproductions/ajqvue/gui/panels/SplashPanel.java
+++ src/com/dandymadeproductions/ajqvue/gui/panels/SplashPanel.java
@@ -9,7 +9,7 @@
 //                   << SplashPanel.java >>
 //
 //=================================================================
-// Copyright (C) 2016 Dana M. Proctor
+// Copyright (C) 2016-2017 Dana M. Proctor
 // Version 1.1 09/21/2016
 //
 // This program is free software; you can redistribute it and/or
