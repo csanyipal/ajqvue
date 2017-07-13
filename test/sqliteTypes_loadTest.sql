b17b2291fe737177fdb9a0b5135d3f4f43f2b669
danap <danap@dandymadeproductions.com>
2017-07-12 18:23:36 -0600

    Added Additional Testing Tables date_types, time_types, and timestamp_types.

--- test/sqliteTypes_loadTest.sql
+++ test/sqliteTypes_loadTest.sql
@@ -5,6 +5,8 @@
 -- =============================================================
 -- Version 07/25/2006 Orignal Load Test SQL configuration for
 --                    Tables of SQLite data types.
+--         07/12/2017 Added Additional Testing Tables date_types, time_types,
+--                    and timestamp_types.
 --      
 -- danap@dandymadeproductions.com
 -- =============================================================
@@ -19,6 +21,10 @@ DROP TABLE IF EXISTS int_types;
 DROP TABLE IF EXISTS real_types;
 DROP TABLE IF EXISTS text_types;
 DROP TABLE IF EXISTS blob_types;
+
+DROP TABLE IF EXISTS date_types;
+DROP TABLE IF EXISTS time_types;
+DROP TABLE IF EXISTS timestamp_types;
 
 -- #############################################################
 -- 
@@ -55,6 +61,28 @@ CREATE TABLE blob_types (
     blob_type BLOB
 );
 
+CREATE TABLE date_types (
+
+--  Table id and creation data entries.
+    data_type_id INTEGER PRIMARY KEY,
+    date_type DATE
+);
+
+CREATE TABLE time_types (
+
+--  Table id and creation data entries.
+    data_type_id INTEGER PRIMARY KEY,
+    time_type TIME
+);
+
+CREATE TABLE timestamp_types (
+
+--  Table id and creation data entries.
+    data_type_id INTEGER PRIMARY KEY,
+    timestamp_type TIMESTAMP
+);
+
+
 -- #############################################################
 --
 -- INSERT INTO TABLE SECTION
@@ -63,9 +91,20 @@ CREATE TABLE blob_types (
 
 INSERT INTO int_types (int_type) VALUES (-2147483648);
 INSERT INTO int_types (int_type) VALUES (2147483647);
-    
+   
 INSERT INTO real_types (real_type) VALUES (-1.1e-39);
 
 INSERT INTO blob_types (blob_type) VALUES (x'0500');
 
 INSERT INTO text_types (text_type) VALUES ('text1');
+
+-- INSERT INTO date_types (date_type) VALUES ('1000-01-01');
+-- INSERT INTO date_types (date_type) VALUES ('9999-12-31');
+
+-- INSERT INTO time_types (time_type) VALUES ('00:00:00');
+-- INSERT INTO time_types (time_type) VALUES ('23:59:59');
+
+INSERT INTO timestamp_types (timestamp_type) VALUES (STRFTIME('%Y-%m-%d %H:%M:%S.%f', 'now', 'localtime'));
+-- INSERT INTO timestamp_types (timestamp_type) VALUES ('1970-01-01 00:00:00');
+
+
