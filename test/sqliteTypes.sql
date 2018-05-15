eedf423c9173bb6f66bbf0ff5ec7223b8111bb41
danap <danap@dandymadeproductions.com>
2018-05-15 12:18:23 -0600

    Changed Table Name sqlitetypes to datatypes. Appears to be JDBC or SQLite Conflict With the Use of the Name, Key Word?

--- test/sqliteTypes.sql
+++ test/sqliteTypes.sql
@@ -7,12 +7,15 @@
 --         07/23/2010 Narrowed to Just Four Types.
 --         04/23/2012 Added View Tables myView.
 --         08/17/2017 Added Auto Increment for Primary Key data_type_id.
+--         05/15/2018 Changed Table Name sqlitetypes, to datatypes. Appears
+--                    to be JDBC or SQLite Conflict With the Use of the Name,
+--                    Key Word?
 --
 -- danap@dandymadeproductions.com
 -- =============================================================
 
-   DROP TABLE IF EXISTS sqlitetypes;
-   CREATE TABLE sqlitetypes (
+   DROP TABLE IF EXISTS datatypes;
+   CREATE TABLE datatypes (
 
 -- Table id and creation data entries.
 
@@ -27,8 +30,8 @@
 );
 
 --
--- View for sqlitetypes
+-- View for datatypes
 --
 
 DROP VIEW IF EXISTS myView;
-CREATE VIEW myView AS SELECT * FROM sqlitetypes;
+CREATE VIEW myView AS SELECT * FROM datatypes;
