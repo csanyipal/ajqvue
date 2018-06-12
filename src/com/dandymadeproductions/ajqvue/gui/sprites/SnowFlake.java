743d389be47d99beffe6c8969c7aca3fdb21ed46
danap <danap@dandymadeproductions.com>
2018-06-12 12:49:15 -0600

    Updated Copyright.

--- src/com/dandymadeproductions/ajqvue/gui/sprites/SnowFlake.java
+++ src/com/dandymadeproductions/ajqvue/gui/sprites/SnowFlake.java
@@ -9,7 +9,7 @@
 //                    << SnowFlake.java >>
 //
 //=================================================================
-// Copyright (C) 2016-2017 Dana M. Proctor
+// Copyright (C) 2016-2018 Dana M. Proctor
 // Version 1.1 09/02/2017
 //
 // This program is free software; you can redistribute it and/or
